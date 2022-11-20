78f091a1b5326e7ae5cf1e91f42ffc161074507c
Sae(Kudou) <konata70@icloud.com>
2022-11-20 23:22:59 +0900

    update: Added Login Route

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,10 +1,17 @@
 import 'package:get/route_manager.dart';
+import 'package:simple_getx_app/pages/login/login_binding.dart';
+import 'package:simple_getx_app/pages/login/login_page.dart';
 import 'package:simple_getx_app/pages/navigation/navigation_binding.dart';
 import 'package:simple_getx_app/pages/navigation/navigation_page.dart';
 import 'package:simple_getx_app/routes/app_routes.dart';
 
 class AppPages {
   static var list = [
+    GetPage(
+      name: AppRoutes.LOGIN,
+      page: () => LoginPage(),
+      binding: LoginBinding(),
+    ),
     GetPage(
       name: AppRoutes.NAVIGATION,
       page: () => NavigationPage(),
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,3 +1,4 @@
 class AppRoutes {
-  static const String NAVIGATION = '/';
+  static const String LOGIN = '/';
+  static const String NAVIGATION = '/navigation';
 }
